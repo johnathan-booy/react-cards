917c0fce7bccb81aabefaadefe9a5cccf69548ed
Johnathan Booy <106690990+johnathan-booy@users.noreply.github.com>
2023-01-17 09:11:19 -0800

    useAxios in Pokedex

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
 import axios from "axios";
 
-// useAxios is a custom React hook that accepts a URL as an input.
-// It makes a GET request to the specified URL using the axios library and stores the response data in a state variable.
-// The hook returns an array containing the response data and a function to make the GET request.
+// useAxios is a custom React hook that accepts a base URL as an input.
+// It returns an array containing the response data and a function to make a GET request to a specified endpoint.
+// The function takes an endpoint as an input and using the axios library makes a GET request to the specified endpoint and the baseURL.
+// The response data is stored in a state variable and returned by the hook.
 // This allows a component to use the hook and access the data and function to make the GET request and update its own state with the response data.
 
-const useAxios = (url) => {
+const useAxios = (baseURL) => {
 	const [data, setData] = useState([]);
 
-	const fetchData = async () => {
+	const fetchData = async (endpoint) => {
+		console.log(endpoint);
+		const url = endpoint === undefined ? baseURL : `${baseURL}/${endpoint}`;
 		const response = await axios.get(url);
-		setData([...data, { ...response.data }]);
+		setData([...data, { ...response.data, id: uuid() }]);
 	};
 
 	return [data, fetchData];
